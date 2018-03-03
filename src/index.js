0edd219f9ea373c76f4727fdb59a8c86ec394f85
petermalcolm <peter.t.malcolm@gmail.com>
2018-03-03 15:22:39 -0500

    render all info in grid

--- src/index.js
+++ src/index.js
@@ -5,30 +5,26 @@ import './index.css';
 const data = require('./data.js');
 
 function Box(props) {
-  return (
-    <div className="box">
-      {props.value}
-    </div>
-  );
+	return (
+		<div className="box">
+			<h2 className="name">{props.person.name}</h2>
+			<span className="age">Age: {props.person.age}</span>
+			<span className="category">Category: {props.person.category}</span>
+		</div>
+	);
 }
 
 class Grid extends React.Component {
-  renderBox(i) {
-    return (
-          <Box
-            value={i}
-          />
-    );
-  }
   render() {
-    return (
-      <div>
-        <div className="grid-row">
-          {this.renderBox('Jon Doe')}
-        </div>
-      </div>
-    );
-  }
+		var namesList = data.data.map(function(person){
+			    return (
+			        <Box person={person} />
+				);
+			});
+		return (
+			<div>{ namesList }</div>
+		);
+	}
 }
 
 ReactDOM.render(
